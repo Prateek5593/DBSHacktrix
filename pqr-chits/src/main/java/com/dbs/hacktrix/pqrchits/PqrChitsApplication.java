9cc5e4d6db2306526e4c3f46d87a6288a4802c9d
shahrukh <shahrukhsaba@gmail.com>
2019-03-30 11:29:45 +0530

    Initial project

---
+++ pqr-chits/src/main/java/com/dbs/hacktrix/pqrchits/PqrChitsApplication.java
@@ -0,0 +1,13 @@
+package com.dbs.hacktrix.pqrchits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrChitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrChitsApplication.class, args);
+	}
+
+}
---
+++ pqr-chits/src/test/java/com/dbs/hacktrix/pqrchits/PqrChitsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbs.hacktrix.pqrchits;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PqrChitsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
