665f8871a3db9e983facb0260000304e52c8cd08
kalva.nithishredyy@99acres.com <kalva.nithishredyy@infoedge.com>
2019-03-30 14:43:15 +0530

    code

--- pqr-chits/src/main/java/com/dbs/hacktrix/pqrchits/domain/User.java
+++ pqr-chits/src/main/java/com/dbs/hacktrix/pqrchits/domain/User.java
@@ -1,11 +1,15 @@
 package com.dbs.hacktrix.pqrchits.domain;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 
 
@@ -27,6 +31,22 @@ public class User {
 	
 	@Column(name="mobile_number")
 	private int mobileNumber;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="IST")
+	@Column(name="creation_time")
+	private Timestamp creationTime;
+	
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="IST")
+	@Column(name="updation_time")
+	private Timestamp updationTime;
+	
 
 	public Integer getId() {
 		return id;
@@ -66,6 +86,38 @@ public class User {
 
 	public void setMobileNumber(int mobileNumber) {
 		this.mobileNumber = mobileNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Timestamp getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Timestamp creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public Timestamp getUpdationTime() {
+		return updationTime;
+	}
+
+	public void setUpdationTime(Timestamp updationTime) {
+		this.updationTime = updationTime;
 	}
 	
 	
