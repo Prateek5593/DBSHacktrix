e555e3b452788286718f791e03f4071552f22659
shahrukh <shahrukhsaba@gmail.com>
2019-03-30 14:25:04 +0530

    cit register service

---
+++ pqr-chits/src/main/java/com/dbs/hacktrix/pqrchits/repository/ChitsUsersRepo.java
@@ -0,0 +1,14 @@
+package com.dbs.hacktrix.pqrchits.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbs.hacktrix.pqrchits.domain.ChitsUserMapping;
+
+@Repository
+public interface ChitsUsersRepo extends JpaRepository<ChitsUserMapping, Integer>{
+	
+	List<ChitsUserMapping> findAll();
+}
